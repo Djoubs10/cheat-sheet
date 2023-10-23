76ce0029cea349ca54d3d9ea42a4310960c922a8
Djoubs10 <69039139+Djoubs10@users.noreply.github.com>
2023-10-23 11:09:36 +0200

    Create MySqlDbContext.cs

---
+++ csharp/Contexts/MySqlDbContext.cs
@@ -0,0 +1,17 @@
+public class InternshipDbContext : DbContext
+{
+    private string _connectionString = "server=; database=; user=; password=";
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseMySQL(_connectionString);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+    }
+
+    public DbSet<Application> Applications { get; set; }
+    public DbSet<Status> Status { get; set; }
+}
