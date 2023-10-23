ecc4280ad27ab4c7fed6207089a509c4da333dde
Djoubs10 <69039139+Djoubs10@users.noreply.github.com>
2023-10-23 13:30:45 +0200

    Update context.cs

--- csharp/database/context.cs
+++ csharp/database/context.cs
@@ -1,1 +1,16 @@
+public class DatabaseContext : DbContext
+{
+    private string _connectionString = "server=; database=; user=; password=";
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseMySQL(_connectionString);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
 
+    }
+
+    public DbSet<Application> Applications { get; set; }
+}
