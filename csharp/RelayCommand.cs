2734575459c2dca15839a8bf270cf88588d4f4dc
Djoubs10 <69039139+Djoubs10@users.noreply.github.com>
2023-10-23 16:19:59 +0200

    Update RelayCommand.cs

--- csharp/RelayCommand.cs
+++ csharp/RelayCommand.cs
@@ -1,18 +1,19 @@
 public class RelayCommand : ICommand
 {
-    public event EventHandler? CanExecuteChanged;
+    public event EventHandler CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
     private Action<object?> _execute;
     private Func<object?, bool>? _canExecute;
     public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
     {
         _execute = execute;
-        _canExecute = canExecute;            
+        _canExecute = canExecute;
     }
     public bool CanExecute(object? parameter)
-    {
-        return _canExecute == null || _canExecute(parameter);   
-    }
-
+        => _canExecute == null || _canExecute(parameter);   
     public void Execute(object? parameter)
     {
         _execute(parameter);
